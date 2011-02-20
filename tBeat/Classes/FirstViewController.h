e27bd8c1a5bb0a15ba021c3ac0a9576f672c458e
Niclas Helbro <niclas.helbro@mancx.com>
2011-02-20 11:37:02 +0100

    First push of tBeat xcode project...again.

---
+++ tBeat/Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  tBeat
+//
+//  Created by Niclas Helbro on 2/20/11.
+//  Copyright 2011 Saddleback College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ tBeat/Classes/tBeatAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  tBeatAppDelegate.h
+//  tBeat
+//
+//  Created by Niclas Helbro on 2/20/11.
+//  Copyright 2011 Saddleback College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tBeatAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
