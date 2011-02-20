a9ef93f9ecb769c9aaddf1337bf7a6f2a4b8d3e9
Niclas Helbro <niclas.helbro@mancx.com>
2011-02-20 17:15:13 +0100

    include all the files too. :@

---
+++ tBeat/Classes/tBeatViewController.h
@@ -0,0 +1,16 @@
+//
+//  tBeatViewController.h
+//  tBeat
+//
+//  Created by Niclas Helbro on 2/20/11.
+//  Copyright 2011 Saddleback College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tBeatViewController : UIViewController {
+
+}
+
+@end
+
